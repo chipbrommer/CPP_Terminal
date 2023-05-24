faea27de40bad4cfeace9b3750e7ff85d2eb26fd
Chip Brommer <chipbrommer@gmail.com>
2023-05-23 19:50:06 -0500

    Add project files.

---
+++ CPP_Terminal/main.cpp
@@ -0,0 +1,27 @@
+﻿// Test file to show use case of CPP Terminal
+
+#include <iostream>
+#include "Source/cpp_terminal.h"
+
+int main()
+{
+	Essentials::Utilities::Terminal term;
+	std::cout << Essentials::Utilities::TerminalVersion << "\n" << std::endl;
+
+	std::string input;
+	std::string result;
+	while (std::getline(std::cin, input))
+	{
+		if (term.ExecuteCommand(input, result) < 0)
+		{
+			std::cout << "ERROR!\n" + term.GetLastError() << std::endl;
+		}
+		else
+		{
+			std::cout << result;
+			result.clear();
+		}
+	}
+
+	return 0;
+}
